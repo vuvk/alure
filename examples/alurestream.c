82331a1b2a9eb48126fd745f24eeb32108a2da94
Chris Robinson <chris.kcat@gmail.com>
2009-10-02 18:59:06 -0700

    Modify alurestream to use the async play API

--- examples/alurestream.c
+++ examples/alurestream.c
@@ -2,12 +2,19 @@
 
 #include "AL/alure.h"
 
+volatile int isdone = 0;
+static void eos_callback(void *unused)
+{
+    isdone = 1;
+    (void)unused;
+}
+
 #define NUM_BUFS 3
 
 int main(int argc, char **argv)
 {
     alureStream *stream;
-    ALuint src, buf[NUM_BUFS];
+    ALuint src;
 
     if(argc < 2)
     {
@@ -29,7 +36,7 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    stream = alureCreateStreamFromFile(argv[1], 19200, NUM_BUFS, buf);
+    stream = alureCreateStreamFromFile(argv[1], 19200, 0, NULL);
     if(!stream)
     {
         fprintf(stderr, "Could not load %s: %s\n", argv[1], alureGetErrorString());
@@ -39,49 +46,18 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    alSourceQueueBuffers(src, NUM_BUFS, buf);
-    alSourcePlay(src);
-    if(alGetError() != AL_NO_ERROR)
+    if(!alurePlayStreamAsync(stream, src, NUM_BUFS, 0, eos_callback, NULL))
     {
-        fprintf(stderr, "Failed to start source!\n");
-        alDeleteSources(1, &src);
-        alureDestroyStream(stream, NUM_BUFS, buf);
-
-        alureShutdownDevice();
-        return 1;
+        fprintf(stderr, "Failed to play stream: %s\n", alureGetErrorString());
+        isdone = 1;
     }
 
-
-    do {
-        ALint state = AL_PLAYING;
-        ALint processed = 0;
-
-        alureSleep(0.01);
-
-        alGetSourcei(src, AL_SOURCE_STATE, &state);
-        alGetSourcei(src, AL_BUFFERS_PROCESSED, &processed);
-        if(processed > 0)
-        {
-            ALuint bufs[NUM_BUFS];
-            alSourceUnqueueBuffers(src, processed, bufs);
-
-            processed = alureBufferDataFromStream(stream, processed, bufs);
-            if(processed <= 0)
-            {
-                do {
-                    alureSleep(0.01);
-                    alGetSourcei(src, AL_SOURCE_STATE, &state);
-                } while(alGetError() == AL_NO_ERROR && state == AL_PLAYING);
-                break;
-            }
-            alSourceQueueBuffers(src, processed, bufs);
-        }
-        if(state != AL_PLAYING)
-            alSourcePlay(src);
-    } while(alGetError() == AL_NO_ERROR);
+    while(!isdone)
+        alureSleep(0.125);
+    alureStopStream(stream, AL_FALSE);
 
     alDeleteSources(1, &src);
-    alureDestroyStream(stream, NUM_BUFS, buf);
+    alureDestroyStream(stream, 0, NULL);
 
     alureShutdownDevice();
     return 0;
