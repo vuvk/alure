7630636b93b24717e438d3b1fc164b6f74940f49
Chris Robinson <chris.kcat@gmail.com>
2011-01-24 19:17:03 -0800

    Split decoders into separate files

---
+++ src/codec_flac.cpp
@@ -0,0 +1,303 @@
+/*
+ * ALURE  OpenAL utility library
+ * Copyright (c) 2009-2010 by Chris Robinson.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "config.h"
+
+#include "main.h"
+
+#include <string.h>
+#include <assert.h>
+
+#include <istream>
+
+#include <FLAC/all.h>
+
+struct flacStream : public alureStream {
+    FLAC__StreamDecoder *flacFile;
+    ALenum format;
+    ALuint samplerate;
+    ALuint blockAlign;
+    ALboolean useFloat;
+
+    std::vector<ALubyte> initialData;
+
+    ALubyte *outBytes;
+    ALuint outLen;
+    ALuint outTotal;
+
+    virtual bool IsValid()
+    { return flacFile != NULL; }
+
+    virtual bool GetFormat(ALenum *fmt, ALuint *frequency, ALuint *blockalign)
+    {
+        *fmt = format;
+        *frequency = samplerate;
+        *blockalign = blockAlign;
+        return true;
+    }
+
+    virtual ALuint GetData(ALubyte *data, ALuint bytes)
+    {
+        outBytes = data;
+        outTotal = 0;
+        outLen = bytes;
+
+        if(initialData.size() > 0)
+        {
+            size_t rem = std::min(initialData.size(), (size_t)bytes);
+            memcpy(data, &initialData[0], rem);
+            outTotal += rem;
+            initialData.erase(initialData.begin(), initialData.begin()+rem);
+        }
+
+        while(outTotal < bytes)
+        {
+            if(pFLAC__stream_decoder_process_single(flacFile) == false ||
+               pFLAC__stream_decoder_get_state(flacFile) == FLAC__STREAM_DECODER_END_OF_STREAM)
+                break;
+        }
+
+        return outTotal;
+    }
+
+    virtual bool Rewind()
+    {
+        if(pFLAC__stream_decoder_seek_absolute(flacFile, 0) != false)
+        {
+            initialData.clear();
+            return true;
+        }
+
+        SetError("Seek failed");
+        return false;
+    }
+
+    flacStream(std::istream *_fstream)
+      : alureStream(_fstream), flacFile(NULL), format(AL_NONE), samplerate(0),
+        blockAlign(0), useFloat(AL_FALSE)
+    {
+        if(!flac_handle) return;
+
+        flacFile = pFLAC__stream_decoder_new();
+        if(flacFile)
+        {
+            if(pFLAC__stream_decoder_init_stream(flacFile, ReadCallback, SeekCallback, TellCallback, LengthCallback, EofCallback, WriteCallback, MetadataCallback, ErrorCallback, this) == FLAC__STREAM_DECODER_INIT_STATUS_OK)
+            {
+                if(InitFlac())
+                {
+                    // all ok
+                    return;
+                }
+
+                pFLAC__stream_decoder_finish(flacFile);
+            }
+            pFLAC__stream_decoder_delete(flacFile);
+            flacFile = NULL;
+        }
+    }
+
+    virtual ~flacStream()
+    {
+        if(flacFile)
+        {
+            pFLAC__stream_decoder_finish(flacFile);
+            pFLAC__stream_decoder_delete(flacFile);
+            flacFile = NULL;
+        }
+    }
+
+private:
+    bool InitFlac()
+    {
+        // We need to decode some data to be able to get the channel count, bit
+        // depth, and sample rate. It also ensures the file has FLAC data, as
+        // the FLAC__stream_decoder_init_* functions can succeed on non-FLAC
+        // Ogg files.
+        outLen = 0;
+        outTotal = 0;
+        while(initialData.size() == 0)
+        {
+            if(pFLAC__stream_decoder_process_single(flacFile) == false ||
+               pFLAC__stream_decoder_get_state(flacFile) == FLAC__STREAM_DECODER_END_OF_STREAM)
+                break;
+        }
+
+        if(initialData.size() > 0)
+            return true;
+        return false;
+    }
+
+    static FLAC__StreamDecoderWriteStatus WriteCallback(const FLAC__StreamDecoder*, const FLAC__Frame *frame, const FLAC__int32 *const buffer[], void *client_data)
+    {
+        flacStream *self = static_cast<flacStream*>(client_data);
+        ALubyte *data = self->outBytes + self->outTotal;
+        ALuint i = 0;
+
+        if(self->format == AL_NONE)
+        {
+            ALuint bps = frame->header.bits_per_sample;
+            if(bps == 24 || bps == 32)
+            {
+                self->format = GetSampleFormat(frame->header.channels, 32, true);
+                if(self->format != AL_NONE)
+                {
+                    self->useFloat = AL_TRUE;
+                    bps = 32;
+                }
+                else bps = 16;
+            }
+            if(self->format == AL_NONE)
+                self->format = GetSampleFormat(frame->header.channels, bps, false);
+            self->blockAlign = frame->header.channels * bps/8;
+            self->samplerate = frame->header.sample_rate;
+        }
+
+        const ALboolean useFloat = self->useFloat;
+        while(self->outTotal < self->outLen && i < frame->header.blocksize)
+        {
+            for(ALuint c = 0;c < frame->header.channels;c++)
+            {
+                if(frame->header.bits_per_sample == 8)
+                    ((ALubyte*)data)[c] = buffer[c][i]+128;
+                else if(frame->header.bits_per_sample == 16)
+                    ((ALshort*)data)[c] = buffer[c][i];
+                else if(frame->header.bits_per_sample == 24)
+                {
+                    if(useFloat)
+                        ((ALfloat*)data)[c] = buffer[c][i] * (1.0/8388607.0);
+                    else
+                        ((ALshort*)data)[c] = buffer[c][i]>>8;
+                }
+                else if(frame->header.bits_per_sample == 32)
+                {
+                    if(useFloat)
+                        ((ALfloat*)data)[c] = buffer[c][i] * (1.0/2147483647.0);
+                    else
+                        ((ALshort*)data)[c] = buffer[c][i]>>16;
+                }
+            }
+            self->outTotal += self->blockAlign;
+            data += self->blockAlign;
+            i++;
+        }
+
+        if(i < frame->header.blocksize)
+        {
+            ALuint blocklen = (frame->header.blocksize-i) *
+                              self->blockAlign;
+            ALuint start = self->initialData.size();
+
+            self->initialData.resize(start+blocklen);
+            data = &self->initialData[start];
+
+            do {
+                for(ALuint c = 0;c < frame->header.channels;c++)
+                {
+                    if(frame->header.bits_per_sample == 8)
+                        ((ALubyte*)data)[c] = buffer[c][i]+128;
+                    else if(frame->header.bits_per_sample == 16)
+                        ((ALshort*)data)[c] = buffer[c][i];
+                    else if(frame->header.bits_per_sample == 24)
+                    {
+                        if(useFloat)
+                            ((ALfloat*)data)[c] = buffer[c][i] * (1.0/8388607.0);
+                        else
+                            ((ALshort*)data)[c] = buffer[c][i]>>8;
+                    }
+                    else if(frame->header.bits_per_sample == 32)
+                    {
+                        if(useFloat)
+                            ((ALfloat*)data)[c] = buffer[c][i] * (1.0/2147483647.0);
+                        else
+                            ((ALshort*)data)[c] = buffer[c][i]>>16;
+                    }
+                }
+                data += self->blockAlign;
+                i++;
+            } while(i < frame->header.blocksize);
+        }
+
+        return FLAC__STREAM_DECODER_WRITE_STATUS_CONTINUE;
+    }
+    static void MetadataCallback(const FLAC__StreamDecoder*,const FLAC__StreamMetadata*,void*)
+    {
+    }
+    static void ErrorCallback(const FLAC__StreamDecoder*,FLAC__StreamDecoderErrorStatus,void*)
+    {
+    }
+
+    static FLAC__StreamDecoderReadStatus ReadCallback(const FLAC__StreamDecoder*, FLAC__byte buffer[], size_t *bytes, void *client_data)
+    {
+        std::istream *stream = static_cast<flacStream*>(client_data)->fstream;
+        stream->clear();
+
+        if(*bytes <= 0)
+            return FLAC__STREAM_DECODER_READ_STATUS_ABORT;
+
+        stream->read(reinterpret_cast<char*>(buffer), *bytes);
+        *bytes = stream->gcount();
+        if(*bytes == 0 && stream->eof())
+            return FLAC__STREAM_DECODER_READ_STATUS_END_OF_STREAM;
+
+        return FLAC__STREAM_DECODER_READ_STATUS_CONTINUE;
+    }
+    static FLAC__StreamDecoderSeekStatus SeekCallback(const FLAC__StreamDecoder*, FLAC__uint64 absolute_byte_offset, void *client_data)
+    {
+        std::istream *stream = static_cast<flacStream*>(client_data)->fstream;
+        stream->clear();
+
+        if(!stream->seekg(absolute_byte_offset))
+            return FLAC__STREAM_DECODER_SEEK_STATUS_ERROR;
+        return FLAC__STREAM_DECODER_SEEK_STATUS_OK;
+    }
+    static FLAC__StreamDecoderTellStatus TellCallback(const FLAC__StreamDecoder*, FLAC__uint64 *absolute_byte_offset, void *client_data)
+    {
+        std::istream *stream = static_cast<flacStream*>(client_data)->fstream;
+        stream->clear();
+
+        *absolute_byte_offset = stream->tellg();
+        return FLAC__STREAM_DECODER_TELL_STATUS_OK;
+    }
+    static FLAC__StreamDecoderLengthStatus LengthCallback(const FLAC__StreamDecoder*, FLAC__uint64 *stream_length, void *client_data)
+    {
+        std::istream *stream = static_cast<flacStream*>(client_data)->fstream;
+        stream->clear();
+
+        std::streampos pos = stream->tellg();
+        if(stream->seekg(0, std::ios_base::end))
+        {
+            *stream_length = stream->tellg();
+            stream->seekg(pos);
+        }
+
+        if(!stream->good())
+            return FLAC__STREAM_DECODER_LENGTH_STATUS_ERROR;
+        return FLAC__STREAM_DECODER_LENGTH_STATUS_OK;
+    }
+    static FLAC__bool EofCallback(const FLAC__StreamDecoder*, void *client_data)
+    {
+        std::istream *stream = static_cast<flacStream*>(client_data)->fstream;
+        return (stream->eof()) ? true : false;
+    }
+};
+static DecoderDecl<flacStream> flacStream_decoder;
---
+++ src/codec_modplug.cpp
@@ -0,0 +1,115 @@
+/*
+ * ALURE  OpenAL utility library
+ * Copyright (c) 2009-2010 by Chris Robinson.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "config.h"
+
+#include "main.h"
+
+#include <string.h>
+#include <assert.h>
+
+#include <istream>
+
+#include <modplug.h>
+
+struct modStream : public alureStream {
+    ModPlugFile *modFile;
+    int lastOrder;
+
+    virtual bool IsValid()
+    { return modFile != NULL; }
+
+    virtual bool GetFormat(ALenum *fmt, ALuint *frequency, ALuint *blockalign)
+    {
+        *fmt = AL_FORMAT_STEREO16;
+        *frequency = 44100;
+        *blockalign = 2 * sizeof(ALshort);
+        return true;
+    }
+
+    virtual ALuint GetData(ALubyte *data, ALuint bytes)
+    {
+        int ret = pModPlug_Read(modFile, data, bytes);
+        if(ret < 0) return 0;
+        return ret;
+    }
+
+    virtual bool Rewind()
+    { return SetOrder(lastOrder); }
+
+    virtual bool SetOrder(ALuint order)
+    {
+        std::vector<char> data(16384);
+        ALuint total = 0;
+        while(total < 2*1024*1024)
+        {
+            fstream->read(&data[total], data.size()-total);
+            if(fstream->gcount() == 0) break;
+            total += fstream->gcount();
+            data.resize(total*2);
+        }
+        data.resize(total);
+
+        ModPlugFile *newMod = pModPlug_Load(&data[0], data.size());
+        if(!newMod)
+        {
+            SetError("Could not reload data");
+            return false;
+        }
+        pModPlug_Unload(modFile);
+        modFile = newMod;
+
+        // There seems to be no way to tell if the seek succeeds
+        pModPlug_SeekOrder(modFile, order);
+        lastOrder = order;
+
+        return true;
+    }
+
+    modStream(std::istream *_fstream)
+      : alureStream(_fstream), modFile(NULL), lastOrder(0)
+    {
+        if(!mod_handle) return;
+
+        std::vector<char> data(16384);
+        ALuint total = 0;
+        while(total < 2*1024*1024)
+        {
+            fstream->read(&data[total], data.size()-total);
+            if(fstream->gcount() == 0) break;
+            total += fstream->gcount();
+            data.resize(total*2);
+        }
+        data.resize(total);
+
+        modFile = pModPlug_Load(&data[0], data.size());
+    }
+
+    virtual ~modStream()
+    {
+        if(modFile)
+            pModPlug_Unload(modFile);
+        modFile = NULL;
+    }
+};
+static DecoderDecl<modStream> modStream_decoder;
